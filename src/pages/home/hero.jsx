362265d362adcbb6e967cda9d706a6d0f028ef1f
fadimeeren <fadime_ern135@outlook.com>
2026-01-08 19:22:24 +0300

    Updated

--- src/pages/home/hero.jsx
+++ src/pages/home/hero.jsx
@@ -7,7 +7,13 @@ const Hero = () => {
   const [error, setError] = useState(null);
 
   useEffect(() => {
-    api.get("/movie/popular");
+    const params = {
+      region: "TR",
+      language: "tr",
+    };
+    api.get("/movie/popular", { params }).then((res) => {
+      const i = Math.round(Math.random() * 19);
+    });
   }, []);
   return (
     <div className="backdrop-blur-m bg-gradient-to-r from-black/40 to-transparent p-8 rounded-3xl border border-white/10 shadow-2xl">
