7dc1f44fdd73b42ace4f7cdc7f89d7bca5886fd5
fadimeeren <fadime_ern135@outlook.com>
2026-01-10 16:21:56 +0300

    Updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,20 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Header from "./components/header";
 import Footer from "./components/footer";
 import Home from "./pages/home";
 import Movie from "./pages/movie";
 import WatchList from "./pages/watch-list";
+import { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { getWatchList } from "./redux/actions";
 
 const App = () => {
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getWatchList());
+  }, []);
+
   return (
     <BrowserRouter>
       <div className="flex flex-col min-h-screen p-5 md:p-10 lg:px-15 xl:px-20">
@@ -14,7 +23,7 @@ const App = () => {
         <main className="flex-1">
           <Routes>
             <Route path="/" element={<Home />} />
-            <Route path="/movie/id:" element={<Movie />} />
+            <Route path="/movie/:id" element={<Movie />} />
             <Route path="/watch-list" element={<WatchList />} />
           </Routes>
         </main>
--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,9 +1,12 @@
 import { Bookmark } from "lucide-react";
+import { useSelector } from "react-redux";
 import { Link } from "react-router-dom";
 
 const Header = () => {
+  const { watchList } = useSelector((store) => store.watchList);
+
   return (
-    <header className="mb-10 flex justify-between items-center backdrop-blur-sm bg-black/30 px-5 md:px-10 lg:px-lg:px-15 xl:px-20 py-4 border border-white/10 shadow-lg rounded-2xl">
+    <header className="mb-10 flex justify-between items-center backdrop-blur-sm bg-black/30 px-5 md:px-10 lg:px-15 xl:px-20 py-4 border border-white/10 shadow-lg rounded-2xl">
       <Link
         to="/"
         className="transition-transform duration-300 hover:scale-105"
@@ -18,7 +21,7 @@ const Header = () => {
         <div className="relative">
           <Bookmark />
           <span className="absolute right-[-13px] top-[-13px] bg-gradient-to-r from-red-500 to-red-600 size-6 grid place-items-center text-xs font-bold rounded-full shadow-lg shadow-red-500/50">
-            4
+            {watchList.length}
           </span>
         </div>
         İzleme Listesi
